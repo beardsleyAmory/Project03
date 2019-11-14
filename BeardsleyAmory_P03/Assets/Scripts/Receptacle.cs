fb07267adc106ffde8cb138f6a808504846e33c9
beardsleyAmory <43458650+beardsleyAmory@users.noreply.github.com>
2019-11-13 22:22:29 -0600

    Receptacle Behavior
    Receptacle reacts to plort object

---
+++ BeardsleyAmory_P03/Assets/Scripts/Receptacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Receptacle : MonoBehaviour
+{
+    public event Action ReceptacleInput = delegate { };
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered Receptacle");
+
+        ReceptacleInput?.Invoke();
+        other.gameObject.SetActive(false);
+    }
+}
