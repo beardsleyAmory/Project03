83d12f59abf586ce933e482d59616508ec48efdf
beardsleyAmory <43458650+beardsleyAmory@users.noreply.github.com>
2019-12-05 01:35:21 -0600

    Star Burst
    Particle burst when plort hits the receptacle is mostly good.

--- BeardsleyAmory_P03/Assets/Scripts/Receptacle.cs
+++ BeardsleyAmory_P03/Assets/Scripts/Receptacle.cs
@@ -7,11 +7,14 @@ public class Receptacle : MonoBehaviour
 {
     public event Action ReceptacleInput = delegate { };
 
+    [SerializeField] ParticleSystem _stars = null;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Triggered Receptacle");
 
         ReceptacleInput?.Invoke();
         other.gameObject.SetActive(false);
+        _stars.Play();
     }
 }
